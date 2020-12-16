9e1d7490d247698a30e9a1199831276220e171d2
Matthew Loewen <matthewlanimates@gmail.com>
2020-12-16 14:10:39 -0500

    check if in range of another node

--- node.py
+++ node.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Node():
 
@@ -13,7 +14,7 @@ class Node():
 
         #range = radius of circle
         self.range = 100
-
+        self.color = self.RED
         # batteryLife = 100
         # sleepSettings = 0
         self.role = name
@@ -21,19 +22,27 @@ class Node():
         # connected = False
         print("making new:", self.role)
 
+    def colorChange(self, color):
+        self.color = color
+
     def show(self, screen, font, toggle):
         #TO DO: Range = to height of sensor on map
         size = self.range
             
-        img = font.render(self.role[0], True, Node.RED)
+        img = font.render(self.role[0], True, self.color)
         screen.blit(img, (self.location[0]-10, self.location[1]-25))
         
         if toggle:
             pygame.draw.circle(screen, (0, 0, 0), [self.location[0], self.location[1]], 5, 255)
-            pygame.draw.circle(screen, (255, 0, 0), [self.location[0], self.location[1]], self.range, 2)
+            pygame.draw.circle(screen, self.color, [self.location[0], self.location[1]], self.range, 2)
 
     def checkNeighbors(self, otherNodes):
-        print("checking")
+        
+        #check distance from "self" to all other nodes
+        for node in otherNodes:
+            dist = math.sqrt(((node.location[0] - self.location[0])**2) + ((node.location[0] - self.location[0])**2))
+            if dist < self.range:
+                print("print something is in range")
 
 class Network():
     # do this later
